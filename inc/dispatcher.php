fdf0d23f2b654d1ea47e25d91fb5ab60285ef528
Marko Heijnen <info@markoheijnen.com>
2014-08-24 23:52:41 +0200

    Add ability to send a specific user a message. See #1
    Includes abstraction of running the actions so we don’t need to
    copy/paste the code.

--- inc/dispatcher.php
+++ inc/dispatcher.php
@@ -46,31 +46,63 @@ class Improved_Heartbeat_Dispatcher {
 	}
 
 
+	public static function user_get( $user_id, $key, $screen_id = null ) {
+		if ( ! $screen_id ) {
+			$screen_id = 'all';
+		}
+
+		$actions = self::get_actions_for_user( $user_id );
+
+		if ( isset( $actions[ $screen_id ], $actions[ $screen_id ][ $key ] ) ) {
+			return $actions[ $screen_id ][ $key ];
+		}
+
+		return false;
+	}
+
+	public static function user_add( $user_id, $key, $value, $screen_id = null, $overwrite = false ) {
+		$action = false;
+
+		if ( ! $screen_id ) {
+			$screen_id = 'all';
+		}
+
+		if ( ! $overwrite ) {
+			$action = self::user_get( $user_id, $key, $screen_id );
+		}
+
+		if ( ! $action ) {
+			$actions = self::get_actions_for_user( $user_id );
+
+			$actions[ $screen_id ][ $key ] = $value;
+
+			return update_user_meta( $user_id, self::option, $actions );
+		}
+
+		return false;
+	}
+
+
 	public function run( $response, $screen_id ) {
 		if ( ! self::$running && doing_action('heartbeat_send') ) {
 			self::$running = true;
 
-			$actions = $this->get_actions();
+			$actions      = $this->get_actions();
+			$user_actions = $this->get_actions_for_user( get_current_user_id() );
+
 
 			if ( $actions ) {
-				// Check for all screens
-				foreach ( $actions['all'] as $key => $value ) {
-					$response[ $key ] = $value;
-
-					unset( $actions['all'][ $key ] );
-				}
-
-				// Check for specific screens
-				if ( isset( $actions[ $screen_id ] ) ) {
-					foreach ( $actions[ $screen_id ] as $key => $value ) {
-						$response[ $key ] = $value;
-
-						unset( $actions[ $screen_id ][ $key ] );
-					}
-				}
+				$response = $this->run_actions( $response, $actions, $screen_id );
 
 				update_option( self::option, $actions );
 			}
+
+			if ( $user_actions ) {
+				$response = $this->run_actions( $response, $user_actions, $screen_id );
+
+				update_user_meta( get_current_user_id(), self::option, $user_actions );
+			}
+
 
 			self::$running = false;
 		}
@@ -79,7 +111,40 @@ class Improved_Heartbeat_Dispatcher {
 	}
 
 
+
 	private static function get_actions() {
 		return get_option( self::option, array( 'all' => array() ) );
 	}
+
+	private static function get_actions_for_user( $user_id ) {
+		$actions = (array) get_user_meta( $user_id, self::option, true );
+
+		if ( ! isset( $actions['all'] ) ) {
+			$actions['all'] = array();
+		}
+
+		return $actions;
+	}
+
+
+	private function run_actions( $response, &$actions, $screen_id ) {
+		// Check for all screens
+		foreach ( $actions['all'] as $key => $value ) {
+			$response[ $key ] = $value;
+
+			unset( $actions['all'][ $key ] );
+		}
+
+		// Check for specific screens
+		if ( isset( $actions[ $screen_id ] ) ) {
+			foreach ( $actions[ $screen_id ] as $key => $value ) {
+				$response[ $key ] = $value;
+
+				unset( $actions[ $screen_id ][ $key ] );
+			}
+		}
+
+		return $response;
+	}
+
 }
