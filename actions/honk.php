5f02ffc9ffc0942775f485d4e58c544c3d50991c
Marko Heijnen <info@markoheijnen.com>
2014-08-24 20:56:29 +0200

    Include Dispatcher in honk example.

--- actions/honk.php
+++ actions/honk.php
@@ -5,7 +5,6 @@ class Improved_Heartbeat_Action_Honk {
 
 	public function __construct() {
 		add_filter( 'json_endpoints', array( $this, 'json_endpoints' ) );
-		add_filter( 'heartbeat_send', array( $this, 'wc_heartbeat_honk' ), 10, 2 );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'wc_heartbeat_honk_script_enqueue' ) );
 		add_action( 'admin_print_footer_scripts', array( $this, 'wc_heartbeat_honk_js' ), 20 );
@@ -14,8 +13,7 @@ class Improved_Heartbeat_Action_Honk {
 	public function json_endpoints( $routes ) {
 		$routes[ '/honk_horn' ] = array(
 			function () {
-				$count = get_option( 'wc-heartbeat-honk', 0 );
-				update_option( 'wc-heartbeat-honk', $count + 1 );
+				Improved_Heartbeat_Dispatcher::add( 'wc-heartbeat-honk', 1 );
 				return array( 'result' => true );
 			},
 			WP_JSON_Server::READABLE
@@ -23,24 +21,6 @@ class Improved_Heartbeat_Action_Honk {
 
 		return $routes;
 	}
-
-	
-	public function wc_heartbeat_honk( $response, $screen_id ) {
-		$do_honk = get_option('wc-heartbeat-honk', 0 );
-
-		if ( $do_honk ) {
-			// Add data in an unique array key -- prefix!
-			$response['wc-heartbeat-honk'] = $do_honk;
-		}
-
-		delete_option('wc-heartbeat-honk');
-
-		// If the above conditions aren't met,
-		// we still pass along the existing $response.
-		return $response;
-		 
-	}
-
 	
 	public function wc_heartbeat_honk_script_enqueue( $hook_suffix ) {
 		// Make sure the JS part of the Heartbeat API is loaded.
